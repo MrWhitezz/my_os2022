5c443b09aaf5d8354d71ce9570145829fd807943
tracer-nju <tracer@nju.edu.cn>
2022-02-25 19:08:51 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:08:51 up  8:37,  1 user,  load average: 0.97, 1.00, 1.00

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <assert.h>
+#include <getopt.h>
+bool show_pid = false;
+bool num_sort = false;
+int parse_args(int argc, char *argv[]);
 
 int main(int argc, char *argv[]) {
+  parse_args(argc, argv);
+
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
   }
   assert(!argv[argc]);
   return 0;
+}
+
+int parse_args(int argc, char *argv[]){
+  const struct option table[] = {
+    {"show-pids"    , no_argument, NULL, 'p'},
+    {"numeric-sort" , no_argument, NULL, 'n'},
+    {"version"      , no_argument, NULL, 'V'}
+  };
+  int opt;
+  while ((opt = getopt_long(argc, argv, "-pnV", table, NULL)) != -1){
+    switch (opt){
+      case 1: printf("You give wrong opt!!!"); break;
+    }
+  }
+
 }
