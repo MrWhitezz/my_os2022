8c3aac91d8347855bea37f5178d4e73f06e357fb
tracer-nju <tracer@nju.edu.cn>
2022-03-01 10:39:23 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:39:23 up 4 days, 7 min,  5 users,  load average: 1.56, 0.89, 0.53

--- libco/co.c
+++ libco/co.c
@@ -1,6 +1,12 @@
 #include "co.h"
 #include <stdlib.h>
 
+#ifdef LOCAL_MACHINE
+  #define debug(...) printf(__VA_ARGS__)
+#else
+  #define debug()
+#endif
+
 struct co {
 };
 
@@ -12,4 +18,9 @@ void co_wait(struct co *co) {
 }
 
 void co_yield() {
+}
+
+
+int main(){
+  debug(dddd);
 }
