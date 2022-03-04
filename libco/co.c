fad473b12ae03740f16717fdff0111738f78b22b
tracer-nju <tracer@nju.edu.cn>
2022-03-04 20:15:34 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:15:34 up 7 days,  9:43,  7 users,  load average: 0.78, 0.75, 0.65

--- libco/co.c
+++ libco/co.c
@@ -101,7 +101,7 @@ void co_yield() {
           current = POOL[i];
           current->status = CO_RUNNING;
           
-          stack_switch_call(&current->stack[STACK_SIZE - 2 * sizeof(uintptr_t)], NULL, 0);
+          stack_switch_call(&current->stack[STACK_SIZE - 16 * sizeof(uintptr_t)], NULL, 0);
           assert(0); 
           stack_switch_call(0, current->func, (uintptr_t)current->arg);
           assert(0); 
