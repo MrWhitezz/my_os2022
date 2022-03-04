ba5ab8032548df220e00be600bba10d2cf508493
tracer-nju <tracer@nju.edu.cn>
2022-03-04 20:40:44 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:40:44 up 7 days, 10:08,  7 users,  load average: 0.75, 0.74, 0.68

--- libco/co.c
+++ libco/co.c
@@ -56,7 +56,7 @@ static inline void stack_store(uintptr_t sp){
   asm volatile (
 #if __x86_64__
     "movq %%rsp, (%0);"
-      : : "d"((uintptr_t)sp): "memory"
+      : : "0"((uintptr_t)sp): "memory"
 #else
     "movl %%esp, (%0);"
       : : "d"((uintptr_t)sp): "memory"
@@ -126,7 +126,7 @@ void co_yield() {
           current = POOL[i];
           current->status = CO_RUNNING;
           
-          // stack_store(current->sp);
+          stack_store(current->sp);
           stack_change(&current->stack[STACK_SIZE - 16 * sizeof(uintptr_t)]);
           ((current->func)(current->arg));
           current->status = CO_DEAD;
