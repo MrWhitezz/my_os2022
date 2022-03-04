2afcb8d1ac924e79e84192157f9b4c441b167c1a
tracer-nju <tracer@nju.edu.cn>
2022-03-04 14:53:09 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:53:09 up 7 days,  4:21,  5 users,  load average: 0.69, 0.81, 0.85

--- libco/co.c
+++ libco/co.c
@@ -1,5 +1,8 @@
 #include "co.h"
 #include <stdlib.h>
+#include <setjmp.h>
+#include <stdint.h>
+#define STACK_SIZE 64
 
 #ifdef LOCAL_MACHINE
   #define debug(...) printf(__VA_ARGS__)
@@ -7,8 +10,36 @@
   #define debug(...)
 #endif
 
-struct co {
+enum co_status {
+  CO_NEW = 1, // 新创建，还未执行过
+  CO_RUNNING, // 已经执行过
+  CO_WAITING, // 在 co_wait 上等待
+  CO_DEAD,    // 已经结束，但还未释放资源
 };
+
+struct co {
+  char *name;
+  void (*func)(void *); // co_start 指定的入口地址和参数
+  void *arg;
+
+  enum co_status status;  // 协程的状态
+  struct co *    waiter;  // 是否有其他协程在等待当前协程
+  jmp_buf        context; // 寄存器现场 (setjmp.h)
+  uint8_t        stack[STACK_SIZE]; // 协程的堆栈
+};
+
+static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg) {
+  asm volatile (
+#if __x86_64__
+    "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
+      : : "b"((uintptr_t)sp), "d"(entry), "a"(arg) : "memory"
+#else
+    "movl %0, %%esp; movl %2, 4(%0); jmp *%1"
+      : : "b"((uintptr_t)sp - 8), "d"(entry), "a"(arg) : "memory"
+#endif
+  );
+}
+
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
   return NULL;
