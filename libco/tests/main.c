9d5ea8383ed8e746810a8551c0115de78623a1bf
tracer-nju <tracer@nju.edu.cn>
2022-03-10 17:04:47 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:04:47 up  5:06,  4 users,  load average: 0.37, 0.32, 0.27

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -16,7 +16,7 @@ static int get_count() {
 
 static void work_loop(void *arg) {
     const char *s = (const char*)arg;
-    for (int i = 0; i < 20000; ++i) {
+    for (int i = 0; i < 200000; ++i) {
         printf("%s%d  ", s, get_count());
         add_count();
         co_yield();
