fcdb3212f4aaded00d852342231f85008804dbcc
tracer-nju <tracer@nju.edu.cn>
2022-03-10 17:37:36 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:37:36 up  5:39,  6 users,  load average: 0.53, 0.68, 0.73

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -38,7 +38,7 @@ static void test_1() {
     struct co *thd7 = co_start("thread-7", work, "d");
     struct co *thd8 = co_start("thread-8", work, "e");
 
-    co_yield();
+    // co_yield();
     co_wait(thd1);
     co_wait(thd2);
     co_wait(thd3);
