4ed7f1ea4c5637a50f3d350942aca8953f9ffcf4
tracer-nju <tracer@nju.edu.cn>
2022-03-11 15:32:54 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:32:54 up  2:21,  3 users,  load average: 0.67, 0.66, 0.56

--- libco/co.c
+++ libco/co.c
@@ -12,11 +12,11 @@
   #define debug(...)
 #endif
 
-#define STACK_SIZE (1024 * (64 + 12))
+#define STACK_SIZE (1024 * (64 + 2))
 
 #define CANARY_SZ  256
 #define STK_OFF    (1024 + 16 * sizeof(uintptr_t))
-#define MAXCO      (128 + 32)
+#define MAXCO      (128 + 8)
 
 #define MAGIC 0x99999999
 #define BOTTOM (STACK_SIZE / sizeof(uint32_t) - 1)
