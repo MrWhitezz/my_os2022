99e6955a2984a14d0e8418a32e13b44b9e056233
tracer-nju <tracer@nju.edu.cn>
2022-03-11 14:26:31 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:26:31 up  1:14,  1 user,  load average: 0.31, 0.45, 0.44

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -126,13 +126,13 @@ static void test_2() {
     co_yield();
     struct co *pd1 = co_start("producer-1", producer, queue);
     struct co *pd2 = co_start("producer-2", producer, queue);
-    struct co *pd3 = co_start("producer-3", producer, queue);
-    co_yield();
-    struct co *pd4 = co_start("producer-4", producer, queue);
-    struct co *pd5 = co_start("producer-5", producer, queue);
-    struct co *pd6 = co_start("producer-6", producer, queue);
-    co_yield();
-    struct co *pd7 = co_start("producer-7", producer, queue);
+    // struct co *pd3 = co_start("producer-3", producer, queue);
+    // co_yield();
+    // struct co *pd4 = co_start("producer-4", producer, queue);
+    // struct co *pd5 = co_start("producer-5", producer, queue);
+    // struct co *pd6 = co_start("producer-6", producer, queue);
+    // co_yield();
+    // struct co *pd7 = co_start("producer-7", producer, queue);
 
     co_yield();
     struct co *cs1 = co_start("consumer-1", consumer, queue);
@@ -146,13 +146,13 @@ static void test_2() {
     co_wait(pd1);
     co_yield();
     co_wait(pd2);
-    co_wait(pd3);
-    co_yield();
-    co_wait(pd4);
-    // printf("debug in test2 line: %d\n", __LINE__);
-    co_wait(pd5);
-    co_wait(pd6);
-    co_wait(pd7);
+    // co_wait(pd3);
+    // co_yield();
+    // co_wait(pd4);
+    // // printf("debug in test2 line: %d\n", __LINE__);
+    // co_wait(pd5);
+    // co_wait(pd6);
+    // co_wait(pd7);
 
     g_running = 0;
 
