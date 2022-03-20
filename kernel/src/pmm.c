e9bb1deff98e14aaab606d16053b9dd2d7ebf8e1
tracer-nju <tracer@nju.edu.cn>
2022-03-20 11:37:54 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:37:54 up 1 day, 14:59,  3 users,  load average: 0.33, 0.40, 0.36

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -1,10 +1,33 @@
 #include <common.h>
 
+// Spinlock
+typedef int spinlock_t;
+#define SPIN_INIT() 0
+
+// int atomic_xchg(int *addr, int newval) {
+//   int result;
+//   asm volatile ("lock xchg %0, %1":
+//     "+m"(*addr), "=a"(result) : "1"(newval) : "memory");
+//   return result;
+// }
+
+void spin_lock(spinlock_t *lk) { while (atomic_xchg(lk, 1))  ; }
+void spin_unlock(spinlock_t *lk) { atomic_xchg(lk, 0); }
+
+typedef struct used_node {
+  Area area;
+  struct used_node* next;
+} used_node;
+
+used_node* H;
+
 static void *kalloc(size_t size) {
-  return NULL;
+  
+  return malloc(size);
 }
 
 static void kfree(void *ptr) {
+  free(ptr);
 }
 
 static void pmm_init() {
