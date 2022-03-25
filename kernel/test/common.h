f94554b3ee5761174a57e66fc59c531ced86faa5
tracer-nju <tracer@nju.edu.cn>
2022-03-25 20:30:31 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:30:31 up 6 days, 23:52,  3 users,  load average: 1.24, 1.24, 1.10

--- kernel/test/common.h
+++ kernel/test/common.h
@@ -1,5 +1,21 @@
 #include <kernel.h>
-#include <klib.h>
-#include <klib-macros.h>
+
+#ifndef KLIB_MACROS_H__
+#define KLIB_MACROS_H__
+
+#define ROUNDUP(a, sz)      ((((uintptr_t)a) + (sz) - 1) & ~((sz) - 1))
+#define ROUNDDOWN(a, sz)    ((((uintptr_t)a)) & ~((sz) - 1))
+#define LENGTH(arr)         (sizeof(arr) / sizeof((arr)[0]))
+#define RANGE(st, ed)       (Area) { .start = (void *)(st), .end = (void *)(ed) }
+#define IN_RANGE(ptr, area) ((area).start <= (ptr) && (ptr) < (area).end)
+
+#define STRINGIFY(s)        #s
+#define TOSTRING(s)         STRINGIFY(s)
+#define _CONCAT(x, y)       x ## y
+#define CONCAT(x, y)        _CONCAT(x, y)
+
+#define panic(s) panic_on(1, s)
+
+#endif
 
 unsigned int nextPower_2(unsigned int x);
