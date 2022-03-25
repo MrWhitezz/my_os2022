e4f9e0c382e3ea4f00a1f9af2448a7a2a0cd25d3
tracer-nju <tracer@nju.edu.cn>
2022-03-25 20:13:48 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:13:48 up 6 days, 23:35,  3 users,  load average: 1.09, 1.09, 0.78

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -18,13 +18,28 @@ static void kfree(void *ptr) {
   list_free(ptr);
 }
 
+#ifndef TEST
 static void pmm_init() {
   list_init();
-  memset(lk, SPIN_INIT(), sizeof(lk)); // abuse of SPIN_INIT() == 1
+  memset(lk, SPIN_INIT(), sizeof(lk)); // abuse of SPIN_INIT() == 0
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
 
 }
+#else
+// 测试代码的 pmm_init ()
+
+#define HEAP_SIZE (128 * (1 << 20))
+static void pmm_init() {
+  char *ptr  = malloc(HEAP_SIZE);
+  heap.start = ptr;
+  heap.end   = ptr + HEAP_SIZE;
+  printf("Got %d MiB heap: [%p, %p)\n", HEAP_SIZE >> 20, heap.start, heap.end);
+
+  list_init();
+  memset(lk, SPIN_INIT(), sizeof(lk)); // abuse of SPIN_INIT() == 1
+}
+#endif
 
 MODULE_DEF(pmm) = {
   .init  = pmm_init,
---
+++ kernel/test/common.h
@@ -0,0 +1,5 @@
+#include <kernel.h>
+#include <klib.h>
+#include <klib-macros.h>
+
+unsigned int nextPower_2(unsigned int x);
