84521cc717737a8b32ed02204e3fbee80903fbea
tracer-nju <tracer@nju.edu.cn>
2022-03-25 20:33:41 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:33:41 up 6 days, 23:55,  3 users,  load average: 1.41, 1.30, 1.14

--- kernel/test/common.h
+++ kernel/test/common.h
@@ -1,4 +1,24 @@
-#include <kernel.h>
+#include <stddef.h>
+#define MODULE(mod) \
+  typedef struct mod_##mod##_t mod_##mod##_t; \
+  extern mod_##mod##_t *mod; \
+  struct mod_##mod##_t
+
+#define MODULE_DEF(mod) \
+  extern mod_##mod##_t __##mod##_obj; \
+  mod_##mod##_t *mod = &__##mod##_obj; \
+  mod_##mod##_t __##mod##_obj
+
+MODULE(os) {
+  void (*init)();
+  void (*run)();
+};
+
+MODULE(pmm) {
+  void  (*init)();
+  void *(*alloc)(size_t size);
+  void  (*free)(void *ptr);
+};
 
 #ifndef KLIB_MACROS_H__
 #define KLIB_MACROS_H__
