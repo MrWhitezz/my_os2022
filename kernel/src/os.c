d78c0629008d36bf778801e114211fc74c1f7685
tracer-nju <tracer@nju.edu.cn>
2022-03-26 16:12:01 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:12:01 up 7 days, 19:33,  4 users,  load average: 1.26, 1.52, 1.47

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -5,21 +5,6 @@ static void os_init() {
   pmm->init();
 }
 
-void do_test_1(){
-  #define ALLOC_SZ 12
-  uint32_t size = 0;
-  for (int i = 0; i < 15000; i++) {
-    void *ptr[ALLOC_SZ];
-    for (int j = 0; j < ALLOC_SZ; ++j){
-      ptr[j] = pmm->alloc(1 << j);
-      // printf("alloc 0x%x at %p\n", 1 << j, ptr[j]);
-      size += 1 << j;
-    }
-    if (i % 1000 == 0) 
-      printf("ptr[0] = %p\n", ptr[0]);
-  }
-  printf("Total size: %d MiB\n", size >> 20);
-}
 
 static void os_run() {
   // for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
