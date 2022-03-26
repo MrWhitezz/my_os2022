74c21b548b2f6b283ed79772a9d12780e9ebbbc7
tracer-nju <tracer@nju.edu.cn>
2022-03-26 16:05:32 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:05:32 up 7 days, 19:27,  4 users,  load average: 1.67, 1.40, 1.36

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -5,13 +5,27 @@ static void os_init() {
   pmm->init();
 }
 
+void do_test_1(){
+  #define ALLOC_SZ 12
+  uint32_t size = 0;
+  for (int i = 0; i < 15000; i++) {
+    void *ptr[ALLOC_SZ];
+    for (int j = 0; j < ALLOC_SZ; ++j){
+      ptr[j] = pmm->alloc(1 << j);
+      // printf("alloc 0x%x at %p\n", 1 << j, ptr[j]);
+      size += 1 << j;
+    }
+    if (i % 1000 == 0) 
+      printf("ptr[0] = %p\n", ptr[0]);
+  }
+  printf("Total size: %d MiB\n", size >> 20);
+}
+
 static void os_run() {
-  // for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
-  //   putch(*s == '*' ? '0' + cpu_current() : *s);
-  // }
-  // for (unsigned x = 0; x < 2147484647; x++) {
-  //   printf("%d is aligned to %d\n", x, nextPower_2(x));
-  // }
+  for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
+    putch(*s == '*' ? '0' + cpu_current() : *s);
+  }
+  do_test_1();
   while (1) ;
 }
 
