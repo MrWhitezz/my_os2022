2ad1137809f0a8eb4af3bfcdf751866c3a09d91d
tracer-nju <tracer@nju.edu.cn>
2022-03-26 15:23:14 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:23:14 up 7 days, 18:44,  3 users,  load average: 1.36, 1.37, 1.38

--- kernel/test/test.c
+++ kernel/test/test.c
@@ -11,13 +11,16 @@ Area heap = {};
 
 void do_test_1(){
   #define ALLOC_SZ 12
+  uint32_t size = 0;
   for (int i = 0; i < 30; i++) {
     void *ptr[ALLOC_SZ];
     for (int j = 0; j < ALLOC_SZ; ++j){
       ptr[j] = pmm->alloc(1 << j);
       printf("alloc 0x%x at %p\n", 1 << j, ptr[j]);
+      size += 1 << j;
     }
   }
+  printf("Total size: %d\n", size);
 }
 
 int main() {
