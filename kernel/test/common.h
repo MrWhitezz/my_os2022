30cc082775cb5f0127aac5635e1c3e39fef757d8
tracer-nju <tracer@nju.edu.cn>
2022-03-26 15:34:35 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:34:35 up 7 days, 18:56,  4 users,  load average: 1.46, 1.30, 1.32

--- kernel/test/common.h
+++ kernel/test/common.h
@@ -70,23 +70,10 @@ MODULE(pmm) {
 
 #define ASNI_FMT(str, fmt) fmt str ASNI_NONE
 
-#define log_write(...) IFDEF(CONFIG_TARGET_NATIVE_ELF, \
-  do { \
-    extern FILE* log_fp; \
-    extern bool log_enable(); \
-    if (log_enable()) { \
-      fprintf(log_fp, __VA_ARGS__); \
-      fflush(log_fp); \
-    } \
-  } while (0) \
-)
-
 #define Log(...) \
   do { \
     printf(__VA_ARGS__); \
-    log_write(__VA_ARGS__); \
   } while (0)
-
 
 #endif
 
