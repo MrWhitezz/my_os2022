de0bcf020b444b9169b3bd3cf23428e9002aaa90
tracer-nju <tracer@nju.edu.cn>
2022-03-30 17:19:32 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:19:32 up 11 days, 20:41,  4 users,  load average: 0.67, 0.63, 0.56

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -1,27 +1,27 @@
 #include <common.h>
 #include <pmm.h>
 
-extern spinlock_t lk[LK_SIZE];
-extern __node_t * head;
-extern uint32_t list_size;
+unsigned int nextPower_2(unsigned int x){
+  x = x - 1;
+  x = x | (x >> 1);
+  x = x | (x >> 2);
+  x = x | (x >> 4);
+  x = x | (x >> 8);
+  x = x | (x >> 16);
+  return x + 1;
+}
+
 
 static void *kalloc(size_t size) {
-  if (size <= sizeof(header_t))
-    size = sizeof(header_t); 
-  else if (size >= MAX_ALLOC)
-    return NULL;
-  
-  return list_alloc(size);
+  return NULL;
 }
 
 static void kfree(void *ptr) {
-  list_free(ptr);
 }
 
 #ifndef TEST
 static void pmm_init() {
-  list_init();
-  memset(lk, SPIN_INIT(), sizeof(lk)); // abuse of SPIN_INIT() == 0
+  // memset(lk, SPIN_INIT(), sizeof(lk)); // abuse of SPIN_INIT() == 0
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
 
