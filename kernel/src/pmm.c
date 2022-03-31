9ca651beb39952d724f13da46201a01c6b225643
tracer-nju <tracer@nju.edu.cn>
2022-04-01 00:16:56 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:16:56 up 13 days,  3:38,  4 users,  load average: 0.38, 0.46, 0.36

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -133,8 +133,6 @@ static void *S_alloc(size_t size){
 }
 
 static void S_free(void *ptr){
-  assert(0);
-  return;
   assert(sizeof(S_node_t) <= 16);
   S_header_t *slab = (S_header_t *)(ROUNDDOWN((uintptr_t)ptr, GPAGE_SZ));
   spinlock_t *lk = &slab->lk;
