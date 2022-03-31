9dda0044ae0ded89ea52b8a8092043ca303599a3
tracer-nju <tracer@nju.edu.cn>
2022-03-31 14:27:12 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:27:12 up 12 days, 17:48,  4 users,  load average: 0.96, 0.85, 0.82

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -64,7 +64,7 @@ static void S_init() {
       Slab[i][j] = (S_header_t *)G_alloc(1, 0);
       Slab[i][j]->unit_size = (1 << (j + 4));
       Slab[i][j]->n_head = (S_node_t *)ROUNDUP(((uintptr_t)Slab[i][j] + sizeof(S_header_t)), Slab[i][j]->unit_size);
-      debug("n_head: %p with unit_size: %d\n", Slab[i][j]->n_head, Slab[i][j]->unit_size);
+      debug("n_head: %p with unit_size: %ld\n", Slab[i][j]->n_head, Slab[i][j]->unit_size);
       Slab[i][j]->S_magic = SMAGIC;
       Slab[i][j]->n_head->next = NULL;
       Slab[i][j]->n_head->size = (GPAGE_SZ - ((uintptr_t)Slab[i][j]->n_head - (uintptr_t)Slab[i][j]));
@@ -79,6 +79,7 @@ static void *S_alloc(size_t size){
   int id = get_slab_index(size);
   assert(Slab[cpu][id]->S_magic == SMAGIC);
   assert(size == Slab[cpu][id]->unit_size);
+  debug("Slab[%d][%d]: %p, size: %ld\n", cpu, id, Slab[cpu][id], Slab[cpu][id]->unit_size);
   spin_lock(&S_lock[cpu][id]);
   S_node_t *node = Slab[cpu][id]->n_head;
   S_node_t *prev = NULL;
