8577ecf3547ed4151121073edcbb7b6455a13575
tracer-nju <tracer@nju.edu.cn>
2022-03-31 15:08:52 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:08:52 up 12 days, 18:30,  4 users,  load average: 1.60, 1.21, 1.04

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -135,7 +135,7 @@ static void S_free(void *ptr){
 static void *G_alloc(size_t npage, size_t rd_sz){
   // alloc npage * GPAGE_SZ, alligned, neglect info_t
   size_t sz = npage * GPAGE_SZ;
-  debug("before lock %p\n", &G_lock);
+  debug("G_alloc: before G lock %p\n", &G_lock);
   spin_lock(&G_lock);
   G_header_t *p = G_head;
   G_header_t *prev = NULL;
@@ -190,13 +190,13 @@ static void *G_alloc(size_t npage, size_t rd_sz){
     p = p->next;
   }
 
-  debug("before unlock %p\n", &G_lock);
+  debug("G_alloc: before G unlock %p\n", &G_lock);
   spin_unlock(&G_lock);
   return (void *)(p);
 }
 
 static void G_free(void *ptr){
-  debug("before lock %p\n", &G_lock);
+  debug("G_free: before G lock %p\n", &G_lock);
   spin_lock(&G_lock);
   assert((ROUNDDOWN((uintptr_t)ptr, GPAGE_SZ)) == (uintptr_t)ptr);
   info_t *info = (info_t *)((uintptr_t)ptr - GPAGE_SZ);
