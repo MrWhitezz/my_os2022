b36bae763cd58a608bfa9fe2d7561e8e7ab09f45
tracer-nju <tracer@nju.edu.cn>
2022-03-31 13:42:05 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:42:05 up 12 days, 17:03,  4 users,  load average: 0.80, 0.74, 0.67

--- kernel/test/test.c
+++ kernel/test/test.c
@@ -40,11 +40,24 @@ void do_test_2(){
   }
 }
 
+void do_test_3(){
+  for (int j = 0; j < 10; ++ j){
+    for (int i = 0; i < 4; ++i){
+      size_t sz = 64 * 1024 * (1 << i);
+      void *ptr = pmm->alloc(sz);
+      printf("alloc 0x%x at %p\n", sz, ptr);
+      assert(ROUNDUP((uintptr_t)ptr, sz) == (uintptr_t)ptr);
+      pmm->free(ptr);
+    }
+  }
+}
+
 int main() {
   pmm->init();
-  for (int i = 0; i < 1; i++)
-    create(do_test_1);
+  // for (int i = 0; i < 1; i++)
+  //   create(do_test_1);
   do_test_2();
-  join(goodbye);
+  do_test_3();
+  // join(goodbye);
   Log("End of main.\n");
 }
