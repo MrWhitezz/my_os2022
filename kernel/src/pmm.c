da27ce80a47a1a0096f83e0cbecbfa2b00c1c426
tracer-nju <tracer@nju.edu.cn>
2022-03-31 15:44:06 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:44:06 up 12 days, 19:05,  4 users,  load average: 1.47, 1.25, 1.07

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -233,8 +233,10 @@ static void G_free(void *ptr){
 bool no_cycle(G_header_t *p){
   debug("before no_cycle loop\n");
   while (p != NULL){
-    if (p == p->next) return false;
+    if (p == p->next) 
+      return false;
     p = p->next;
+    debug("p: %p, p->next: %p\n", p, p->next);
   }
   debug("after no_cycle loop\n");
   return true;
