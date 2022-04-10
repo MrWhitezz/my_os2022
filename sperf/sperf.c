d49801f3fec12fd50539b4d3428076f6e4bb7402
tracer-nju <tracer@nju.edu.cn>
2022-04-10 15:16:10 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:16:10 up 5 days, 17:59,  6 users,  load average: 0.27, 0.42, 0.47

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -13,15 +13,17 @@ void print_argv(char *argv[]){
 int main(int argc, char *argv[]) {
   // char *exec_argv[] = { "strace", "ls", NULL, };
   argv[0] = "strace";
-  print_argv(argv);
-  print_argv(environ);
+  // print_argv(argv);
+  // print_argv(environ);
+
   // char *exec_argv[] = {"ls", "ls", NULL, };
+  // char *exec_envp[] = { "PATH=/bin", NULL, };
   char **exec_argv = argv;
-  char *exec_envp[] = { "PATH=/bin", NULL, };
+  char **exec_envp = environ;
 
   execve("strace",          exec_argv, exec_envp);
-  execve("/bin/strace",     exec_argv, exec_envp);
-  execve("/usr/bin/strace", exec_argv, exec_envp);
-  perror(argv[0]);
-  exit(EXIT_FAILURE);
+  // execve("/bin/strace",     exec_argv, exec_envp);
+  // execve("/usr/bin/strace", exec_argv, exec_envp);
+  // perror(argv[0]);
+  // exit(EXIT_FAILURE);
 }
