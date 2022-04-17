9f03a7a115319d2d7e3f804ab2a76992ef4c6187
tracer-nju <tracer@nju.edu.cn>
2022-04-17 20:07:47 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:07:47 up 2 days,  5:58,  3 users,  load average: 0.54, 0.72, 0.58

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+
+static bool is_func(char *str) {
+  while (*str && *str != ' ') str++;
+  if (!str[0] || str[0] != 'i') return false; 
+  if (!str[1] || str[1] != 'n') return false; 
+  if (!str[2] || str[2] != 't') return false; 
+  if (!str[3] || str[3] != ' ') return false; 
+  return true;
+}
 
 int main(int argc, char *argv[]) {
   static char line[4096];
@@ -10,5 +20,10 @@ int main(int argc, char *argv[]) {
       break;
     }
     printf("Got %zu chars.\n", strlen(line)); // ??
+    if (is_func(line)) {
+      printf("It's a function.\n");
+    } else {
+      printf("It's not a function.\n");
+    }
   }
 }
