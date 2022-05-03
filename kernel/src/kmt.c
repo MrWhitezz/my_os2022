42a8edab200dfdf96f6af7da2c09f272170f79d6
tracer-nju <tracer@nju.edu.cn>
2022-05-03 15:30:56 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:30:56 up 1 day,  4:17,  4 users,  load average: 0.58, 0.68, 0.60

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -56,16 +56,16 @@ static void spin_lock(spinlock_t *lk){
   }
 
   long long cnt = 0;
-  int prt = 0;
+  // int prt = 0;
   while(atomic_xchg(&lk->locked, 1)) {
     // debug("cpu %d spin_lock %s\n", cpu_current(), lk->name);
     cnt++;
     panic_on(cnt > 1000000000LL, "spin_lock deadlock");
-    if (cnt > 100000000LL && prt == 0) {
-      debug("spinlk %s is held by cpu %d\n", lk->name, lk->cpu);
-      debug("this cpu is %d cannot acquire\n", cpu_current());
-      prt = 1;
-    }
+    // if (cnt > 100000000LL && prt == 0) {
+    //   debug("spinlk %s is held by cpu %d\n", lk->name, lk->cpu);
+    //   debug("this cpu is %d cannot acquire\n", cpu_current());
+    //   prt = 1;
+    // }
   }
   __sync_synchronize();
 
@@ -75,8 +75,6 @@ static void spin_lock(spinlock_t *lk){
 
 static void spin_unlock(spinlock_t *lk) {
   if(!holding(lk)) {
-    // debug("lk %s : %d is held by %d\n", lk->name, lk->locked, lk->cpu);
-    // debug("this cpu %d is trying to unlock\n", cpu_current());
     panic("release(spin_unlock)");
   }
 
