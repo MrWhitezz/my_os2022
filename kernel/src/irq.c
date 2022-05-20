ed8da6c417bdd4768ef55f8fcb6c089ba5112081
tracer-nju <tracer@nju.edu.cn>
2022-05-20 16:07:34 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:07:34 up 11 days,  6:15,  6 users,  load average: 1.79, 1.47, 1.27

--- kernel/src/irq.c
+++ kernel/src/irq.c
@@ -7,6 +7,14 @@ static Context *pagefault(Event ev, Context *ctx) {
   return NULL;
 }
 
+static Context *syscall(Event ev, Context *ctx) {
+  if (ev.event != EVENT_SYSCALL) return NULL;
+  assert(ev.event == EVENT_SYSCALL);
+  panic("syscall not implemented");
+  return NULL;
+}
+
 void irq_init() {
   os->on_irq(100, EVENT_PAGEFAULT, pagefault);
+  os->on_irq(200, EVENT_SYSCALL, syscall);
 }
