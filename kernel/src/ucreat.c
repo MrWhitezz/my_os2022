8ff1366d5a48c49d1659d149293b4c700a9936bd
tracer-nju <tracer@nju.edu.cn>
2022-05-22 21:33:35 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:33:35 up 13 days, 11:41,  7 users,  load average: 1.58, 1.36, 1.26

--- kernel/src/ucreat.c
+++ kernel/src/ucreat.c
@@ -3,8 +3,10 @@
 
 int *ucreate_(task_t *task, const char *name) {
   assert(task != NULL);
-  protect(&task->as);
   AddrSpace *as = &task->as;
+  debug("[as] st: %p ed: %p\n", as->area.start, as->area.end);
+  debug("[as] pgsz: %d\n", as->pgsize);
+  protect(&task->as);
   debug("[as] st: %p ed: %p\n", as->area.start, as->area.end);
   debug("[as] pgsz: %d\n", as->pgsize);
 
