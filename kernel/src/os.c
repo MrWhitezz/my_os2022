33691d19fb35ab3582943b8909df65a4776ac7d3
tracer-nju <tracer@nju.edu.cn>
2022-05-22 20:23:16 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:23:16 up 13 days, 10:30,  7 users,  load average: 1.39, 1.29, 1.12

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -46,18 +46,18 @@ void get_sum(void *arg) {
     debug("sum: %d, cpu: %d\n", sum, cpu_current());
 }
 
-static void tty_reader(void *arg) {
-  device_t *tty = dev->lookup(arg);
-  char cmd[128], resp[128], ps[16];
-  sprintf(ps, "(%s) $ ", arg);
-  while (1) {
-    tty->ops->write(tty, 0, ps, strlen(ps));
-    int nread = tty->ops->read(tty, 0, cmd, sizeof(cmd) - 1);
-    cmd[nread] = '\0';
-    sprintf(resp, "tty reader task: got %d character(s).\n", strlen(cmd));
-    tty->ops->write(tty, 0, resp, strlen(resp));
-  }
-}
+// static void tty_reader(void *arg) {
+//   device_t *tty = dev->lookup(arg);
+//   char cmd[128], resp[128], ps[16];
+//   sprintf(ps, "(%s) $ ", arg);
+//   while (1) {
+//     tty->ops->write(tty, 0, ps, strlen(ps));
+//     int nread = tty->ops->read(tty, 0, cmd, sizeof(cmd) - 1);
+//     cmd[nread] = '\0';
+//     sprintf(resp, "tty reader task: got %d character(s).\n", strlen(cmd));
+//     tty->ops->write(tty, 0, resp, strlen(resp));
+//   }
+// }
 #endif
 
 
@@ -93,8 +93,8 @@ static void os_init() {
     kmt->create(os_tsk_alloc(), name, waste_time, NULL);
   }
 
-  kmt->create(os_tsk_alloc(), "tty_reader", tty_reader, "tty1");
-  kmt->create(os_tsk_alloc(), "tty_reader", tty_reader, "tty2");
+  // kmt->create(os_tsk_alloc(), "tty_reader", tty_reader, "tty1");
+  // kmt->create(os_tsk_alloc(), "tty_reader", tty_reader, "tty2");
 
 #endif
 }
