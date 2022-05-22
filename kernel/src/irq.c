9a4c0401bd5889f659b32ecf358ae9a7e0cfd5ed
tracer-nju <tracer@nju.edu.cn>
2022-05-22 21:52:52 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:52:52 up 13 days, 12:00,  7 users,  load average: 0.77, 0.71, 0.88

--- kernel/src/irq.c
+++ kernel/src/irq.c
@@ -20,7 +20,8 @@ static Context *pagefault(Event ev, Context *ctx) {
 	AddrSpace *as = &(tcurrent->as);
 	void *va = (void *)ROUNDDOWN(ev.ref, as->pgsize);
 	void *pa = pmm->alloc(as->pgsize);
-	assert(va >= as->area.start && va < as->area.end);
+	assert(va >= as->area.start);
+	assert(va < as->area.end);
 	pgmap(tcurrent, va, pa);
 	printf("pf: %p by %p\n", ev.ref, ctx->rip);
   // panic("pagefault not implemented");
