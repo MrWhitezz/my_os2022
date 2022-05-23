61aa578cc9be32b5ae01bfe1e3c9f29d67f689b3
tracer-nju <tracer@nju.edu.cn>
2022-05-23 11:10:05 +0800

    Linux xyq-Lenovo-Legion-Y7000P2020H 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:10:05 up 14 days,  1:17,  7 users,  load average: 0.33, 0.47, 0.57

--- kernel/src/uproc.c
+++ kernel/src/uproc.c
@@ -23,8 +23,50 @@ static int u_fork(task_t *t) {
   return 0;
 }
 
+static int u_wait (task_t *t, int *status) {
+  panic("wait not implemented");
+  return 0;
+}
+
+static int u_exit(task_t *t, int status) {
+  panic("exit not implemented");
+  return 0;
+}
+
+static int u_kill(task_t *t, int pid) {
+  panic("kill not implemented");
+  return 0;
+}
+
+static void *u_mmap(task_t *t, void *addr, int length, int prot, int flags) {
+  panic("mmap not implemented");
+  return 0;
+}
+
+static int u_getpid(task_t *t) {
+  panic("getpid not implemented");
+  return 0;
+}
+
+static int u_sleep(task_t *t, int seconds) {
+  panic("sleep not implemented");
+  return 0;
+}
+
+static int64_t u_uptime(task_t *t) {
+  panic("uptime not implemented");
+  return 0;
+}
+
 MODULE_DEF(uproc) = {
-  .init = uproc_init,
-  .kputc = u_kputc,
-  .fork = u_fork,
+  .init   = uproc_init,
+  .kputc  = u_kputc,
+  .fork   = u_fork,
+  .wait   = u_wait,
+  .exit   = u_exit,
+  .kill   = u_kill,
+  .mmap   = u_mmap,
+  .getpid = u_getpid,
+  .sleep  = u_sleep,
+  .uptime = u_uptime,
 };
